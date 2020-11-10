5b8201bf4db4b57ea26012d0613415b25b7df3a7
Miika Alikirri <miika.alik@gmail.com>
2020-11-10 02:40:15 +0200

    Modified build script to fix the header issues

--- build_third_party.sh
+++ build_third_party.sh
@@ -11,3 +11,7 @@ cd third_party/yle-tekstitv
             --disable-shared-lib
 make
 cp build/lib/libtekstitv.a include/tekstitv.h ../../lib
+cd ../../lib
+# Golang has type as a reserved word so we need to this
+sed -i 's/html_item_type type/html_item_type i_type/g' tekstitv.h
+sed -i 's/item\.type/item.\i_type/g' tekstitv.h
